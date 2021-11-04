5c7492bd4383d99bce958708093d46f2d2ad39b2
FLVSCODER1 <93746004+FLVSCODER1@users.noreply.github.com>
2021-11-04 19:52:24 -0400

    Add files via upload

---
+++ code.py
@@ -0,0 +1,47 @@
+def main():
+
+
+
+
+
+
+
+
+
+
+
+answer = input("Would you like to play a game? (yes/no)")
+
+
+
+
+
+if(answerA == "yes"):
+print("Cool! You have __ guesses")
+
+if(answer2 >= "12"):
+answerB = input("Too high... want another guess?")
+
+if answerB == "yes"):
+
+print("cool! You have __ Guesses
+
+
+n = 0 
+
+while (answer2 != 12):
+
+print(n)
+
+n = n + 1
+
+else: 
+
+print("Too Bad... Play Later?")
+
+main()
+
+
+
+
+
